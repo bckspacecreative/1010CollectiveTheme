a6eb76706cc4e4a5ba9c86e93dab929fb1a2f627
Jeffrey Clark <jeff@the1010collective.com>
2012-10-17 17:00:32 -0700

    header changed

--- 1010CollectiveTheme/header.php
+++ 1010CollectiveTheme/header.php
@@ -33,7 +33,6 @@
                     
                     <div class="navigation">
                         <?php wp_nav_menu( array( 'menu' => 'primary' )); ?>
-<?php echo "change"; ?>
                     </div><!-- end navigation -->
                 </div> <!-- end wrapper -->
          </div><!-- end header wrap -->
